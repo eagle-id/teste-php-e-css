bbd264b328ed40b02bbdb5021e8acada0d54868f
dev4 <programacao4@eagledigital.com.br>
2017-06-26 17:39:38 -0300

    completed functions list / list_all

--- inc/database.php
+++ inc/database.php
@@ -22,20 +22,47 @@ function close_database($conn)
     }
 }
 
-/*
- * Preencha as funções abaixo;
- * Pode alterar os parametros se preferir;
-*/
-
 function find( $table = null, $id = null ) 
 {  
-    // stuff
+    $database = open_database();
+    $found = null;
+    try {
+        if ($id) {
+            $sql = "SELECT * FROM " . $table . " WHERE id = " . $id;
+            $result = $database->query($sql);
+
+            if ($result->num_rows > 0) {
+                $found = $result->fetch_assoc();
+            }
+        } else {
+            $sql = "SELECT * FROM " . $table;
+            $result = $database->query($sql);
+
+            if ($result->num_rows > 0) {
+                $numRows = $result->num_rows;
+                for ($i=0; $i < $numRows; $i++) { 
+                    $found[$i] = $result->fetch_assoc();
+                }
+            }
+        }
+    } catch (Exception $e) {
+        $_SESSION['message'] = $e->GetMessage();
+        $_SESSION['type'] = 'danger';
+    }
+
+    close_database($database);
+    
+    return $found;
 }
 
 function find_all( $table ) 
 {
-    // stuff
+    return find($table);
 }
+
+/*
+ * Preencha as funções abaixo;
+*/
 
 function save($table = null, $data = null) 
 {
